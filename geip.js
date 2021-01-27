bff49dc53ebc28fd6c432b32fba58c874a7b9d27
rgamin1 <33296954+rgamin1@users.noreply.github.com>
2021-01-27 14:37:00 -0500

    updates

---
+++ geip.js
@@ -0,0 +1,19 @@
+const headers = new Headers()
+headers.append("Content-Type", "application/json")
+
+var getUrl = window.location;
+var baseUrl = getUrl .protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
+
+const body = {
+  "user_agent": navigator.userAgent,
+  "url": baseUrl
+}
+ 
+const options = {
+  method: "POST",
+  headers,
+  mode: "cors",
+  body: JSON.stringify(body),
+}
+ 
+fetch("https://en2vswvr7591m4q.m.pipedream.net", options)
